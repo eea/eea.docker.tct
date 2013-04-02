543b35993667456bfa167689556d159117827b5d
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2013-04-02 15:17:51 +0300

    initial models

---
+++ src/nbsap/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class AichiGoal(models.Model):
+    raise NotImplementedError
+
+class AichiTarget(models.Model):
+    raise NotImplementedError
+
+class AichiIndicator(models.Model):
+    raise NotImplementedError
+
+class NationalObjective(models.Model):
+    raise NotImplementedError
+
+class EuTarget(models.Model):
+    raise NotImplementedError
+
+class EuAction(models.Model):
+    raise NotImplementedError
