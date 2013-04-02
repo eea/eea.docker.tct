d15a0d11af1576eaf62e2059c82c0abed73a5b1f
Mihai Tabara <tabara.mihai@gmail.com>
2013-04-02 18:07:44 +0300

    complete aichi schema

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
- 
+
 from setuptools import setup
- 
+
 VERSION = '0.0.1'
- 
+
 setup(
     name='nbsap',
     version=VERSION,
--- src/nbsap/models.py
+++ src/nbsap/models.py
@@ -1,13 +1,64 @@
 from django.db import models
 
-class AichiGoal(models.Model):
-    pass
-
-class AichiTarget(models.Model):
-    pass
+class Scale(models.Model):
+    name = models.CharField(max_length=30)
 
 class AichiIndicator(models.Model):
-    pass
+    LEVEL_CHOICES = (
+        ('low', 'Low'),
+        ('med', 'Medium'),
+        ('hig', 'High'),
+    )
+
+    title = models.CharField("Operational Indicator",
+                            max_length=250)
+    question = models.CharField("Communication Question",
+                            max_length=250)
+    head_indicator = models.CharField("Headline Indicator",
+                            max_length=250)
+    sub_indicator = models.CharField("Indicator Sub-topics",
+                            max_length=250)
+    classification = models.CharField("Operational Classification",
+                            max_length=250)
+    description = models.TextField()
+    sensitivity = models.CharField("Sensitivity (can it be used to make assessment by 2015?)",
+                                   max_length=3,
+                                   choices=LEVEL_CHOICES)
+    scale = models.ForeignKey(Scale)
+    validity = models.CharField("Scientific Validity",
+                                   max_length=3,
+                                   choices=LEVEL_CHOICES)
+    ease_of_communication = models.CharField("How easy can it be communicated?",
+                                   max_length=3,
+                                   choices=LEVEL_CHOICES)
+    sources = models.CharField("Data Sources",
+                            max_length=250)
+    requirements = models.TextField()
+    measurer = models.CharField("Who's responsible for measuring?",
+                            max_length=250)
+    conventions = models.CharField("Other conventions/processes using indicator",
+                            max_length=250)
+    links = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+class AichiTarget(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    indicators = models.ForeignKey(AichiIndicator, related_name="relevant_target")
+    other_indicators = models.ForeignKey(AichiIndicator, related_name="other_targets")
+
+    def __unicode__(self):
+        return self.title
+
+class AichiGoal(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    targets = models.ForeignKey(AichiTarget)
+
+    def __unicode__(self):
+        return self.title
 
 class NationalAction(models.Model):
     title = models.CharField(max_length=255)
@@ -32,9 +83,6 @@ class NationalObjective(models.Model):
 class EuAction(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    objectives = models.ForeignKey('self',
-                                   null=True,
-                                   blank=True)
 
     def __unicode__(self):
         return self.title
