31fce573106ff60d9ac0a3639a8bf318fc8c8284
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2013-04-03 18:28:38 +0300

    aichi goals

--- src/nbsap/views.py
+++ src/nbsap/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from nbsap import models
 
 
 def goals(request, code):
+    current_goal = get_object_or_404(models.AichiGoal, code=code)
     goals = models.AichiGoal.objects.order_by('code').all()
     return render(request, 'goals.html',
                   {'goals': goals,
-                   'current_page': code})
+                   'current_page': code,
+                   'current_goal': current_goal,
+                  })
 
 def eu_strategy(request):
     return HttpResponse("eu_strategy")
