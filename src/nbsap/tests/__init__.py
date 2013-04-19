0ed373caa471933c710d75f19a64ffd52fece1b4
Iulia Pasov <iulia.pasov@eaudeweb.ro>
2013-04-19 19:56:28 +0300

    Mapping tests and delete js confirmation

--- src/nbsap/tests/__init__.py
+++ src/nbsap/tests/__init__.py
@@ -1,3 +1,4 @@
 from actions import *
 from objectives import *
 from subobjectives import *
+from national_strategy import *
---
+++ src/nbsap/tests/national_strategy.py
@@ -0,0 +1,173 @@
+from django.test.client import Client
+from django.utils import unittest
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from nbsap import models
+
+
+class NationalStrategyTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('admin', 'admin@admin.com', 'q')
+        self.client = Client()
+        call = self.client.post('/accounts/login/', {'username': 'admin',
+                                                     'password': 'q'})
+
+    def test_list_national_strategies(self):
+        response = self.client.get('/administration/mapping/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['strategies']), 47)
+        content = response.content
+        self.assertIn("Delete", content)
+        self.assertIn("Add Mapping", content)
+        self.assertIn("Edit", content)
+
+    def test_add_national_strategy_only_with_required_fields(self):
+        mydata = {
+            'nat_objective': 2,
+            'aichi_goal': 'a',
+            'aichi_target': 4
+        }
+
+        response = self.client.get('/administration/mapping/add')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post('/administration/mapping/add', mydata)
+        self.assertEqual(response.status_code, 302)
+
+        strategies = models.NationalStrategy.objects.all()
+        added_strategy = strategies[len(strategies)- 1]
+        self.assertEqual(added_strategy.objective.id, mydata['nat_objective'])
+        self.assertEqual(added_strategy.relevant_target.id,
+                         mydata['aichi_target'])
+        self.assertEqual(added_strategy.relevant_target.get_parent_goal().code,
+                         mydata['aichi_goal'])
+
+    def test_add_national_strategy_only_with_incorrect_goal(self):
+        mydata = {
+            'nat_objective': 2,
+            'aichi_goal': 'b',
+            'aichi_target': 4
+        }
+        response = self.client.post('/administration/mapping/add', mydata)
+        self.assertEqual(response.status_code, 302)
+        strategies = models.NationalStrategy.objects.all()
+        added_strategy = strategies[len(strategies) - 1]
+        self.assertFalse(added_strategy.relevant_target.get_parent_goal().code  ==  mydata['aichi_goal'])
+
+    def test_add_national_strategy_only_with_all_fields(self):
+        mydata = {
+            'nat_objective': 2,
+            'aichi_goal': 'b',
+            'aichi_target': 5,
+            'other_targets': [1,3],
+            'eu_targets': [4],
+            'eu_actions': [36]
+        }
+
+        response = self.client.post('/administration/mapping/add', mydata)
+        self.assertEqual(response.status_code, 302)
+
+        strategies = models.NationalStrategy.objects.all()
+        added_strategy = strategies[len(strategies)- 1]
+        self.assertEqual(added_strategy.objective.id, mydata['nat_objective'])
+        self.assertEqual(added_strategy.relevant_target.id,
+                         mydata['aichi_target'])
+        self.assertEqual(added_strategy.relevant_target.get_parent_goal().code,
+                         mydata['aichi_goal'])
+
+        for target in added_strategy.other_targets.all():
+            self.assertIn(target.id, mydata['other_targets'])
+        for target in added_strategy.eu_targets.all():
+            self.assertIn(target.id, mydata['eu_targets'])
+        for target in added_strategy.eu_actions.all():
+            self.assertIn(target.id, mydata['eu_actions'])
+
+        self.assertEqual(len(added_strategy.other_targets.all()),
+                         len(mydata['other_targets']))
+        self.assertEqual(len(added_strategy.eu_targets.all()),
+                         len(mydata['eu_targets']))
+        self.assertEqual(len(added_strategy.eu_actions.all()),
+                         len(mydata['eu_actions']))
+
+
+
+    def test_delete_national_strategy(self):
+        mydata = {
+            'nat_objective': 2,
+            'aichi_goal': 'b',
+            'aichi_target': 5,
+            'other_targets': [1,3],
+            'eu_targets': [4],
+            'eu_actions': [36]
+        }
+        response = self.client.post('/administration/mapping/add', mydata)
+        self.assertEqual(response.status_code, 302)
+
+        strategies = models.NationalStrategy.objects.all()
+        added_strategy = strategies[len(strategies)- 1]
+        my_id = added_strategy.id
+        response = self.client.get('/administration/mapping/%s/delete' %
+                                   (str(my_id)))
+        strategies = models.NationalStrategy.objects.all().filter(id=my_id)
+        if strategies:
+          self.assertEqual(1,2)
+
+
+
+    def test_edit_national_strategy_with_all(self):
+        mydata = {
+            'nat_objective': 2,
+            'aichi_goal': 'b',
+            'aichi_target': 5,
+            'other_targets': [1,3],
+            'eu_targets': [4],
+            'eu_actions': [36]
+        }
+        response = self.client.get('/administration/mapping/1/add')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post('/administration/mapping/1/add', mydata)
+
+        strategy = models.NationalStrategy.objects.all().filter(id=1)[0]
+        self.assertEqual(strategy.objective.id, mydata['nat_objective'])
+        self.assertEqual(strategy.relevant_target.id, mydata['aichi_target'])
+
+        for target in strategy.other_targets.all():
+            self.assertIn(target.id, mydata['other_targets'])
+        for target in strategy.eu_targets.all():
+            self.assertIn(target.id, mydata['eu_targets'])
+        for target in strategy.eu_actions.all():
+            self.assertIn(target.id, mydata['eu_actions'])
+
+        self.assertEqual(len(strategy.other_targets.all()),
+                         len(mydata['other_targets']))
+        self.assertEqual(len(strategy.eu_targets.all()),
+                         len(mydata['eu_targets']))
+        self.assertEqual(len(strategy.eu_actions.all()),
+                         len(mydata['eu_actions']))
+
+    def test_edit_national_strategy_with_required_fields(self):
+        mydata = {
+            'nat_objective': 2,
+            'aichi_goal': 'b',
+            'aichi_target': 5,
+        }
+        response = self.client.get('/administration/mapping/1/add')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post('/administration/mapping/1/add', mydata)
+
+        strategy = models.NationalStrategy.objects.all().filter(id=1)[0]
+        self.assertEqual(strategy.objective.id, mydata['nat_objective'])
+        self.assertEqual(strategy.relevant_target.id, mydata['aichi_target'])
+
+        self.assertEqual(len(strategy.other_targets.all()), 0)
+        self.assertEqual(len(strategy.eu_targets.all()), 0)
+        self.assertEqual(len(strategy.eu_actions.all()), 0)
+
+
+
+    def tearDown(self):
+        self.user.delete()
