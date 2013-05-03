9866a3e05f2d8892fcdc8756fc6d2d3e0a4ed90d
Iulia Pasov <iulia.pasov@eaudeweb.ro>
2013-05-03 16:45:13 +0300

    Added translated messages for logout

--- src/nbsap/views/auth.py
+++ src/nbsap/views/auth.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.contrib import messages
+
+from django.utils.translation import ugettext_lazy as _
 
 def logout_view(request):
+    messages.success(request, _("Successfully logged out") + "")
     logout(request)
     return redirect('goals')
