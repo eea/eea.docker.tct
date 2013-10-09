8cdb8a79778eb3d3fc78f8eca92a8673802941d8
Alex Morega <alex@grep.ro>
2013-10-09 15:02:11 +0300

    expose label of default language in templates

--- src/nbsap/context_processors.py
+++ src/nbsap/context_processors.py
@@ -2,8 +2,10 @@ from django.conf import settings
 import re
 
 def nbsap_admin(request):
+    default_lang = settings.LANGUAGE_CODE
     return {
         'EU_STRATEGY': getattr(settings, 'EU_STRATEGY', False),
         'SITE_HEADER': getattr(settings, 'SITE_HEADER', 'NBSAP'),
-        'DEFAULT_LANGUAGE': settings.LANGUAGE_CODE,
+        'DEFAULT_LANGUAGE': default_lang,
+        'DEFAULT_LANGUAGE_LABEL': dict(settings.LANGUAGES)[default_lang],
     }
