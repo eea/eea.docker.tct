9d6e0434f6d5e4443c5e5e27cd024f36a413f14a
Alex Morega <alex@grep.ro>
2013-10-09 14:54:58 +0300

    show values in default language on the right side

--- src/nbsap/context_processors.py
+++ src/nbsap/context_processors.py
@@ -5,4 +5,5 @@ def nbsap_admin(request):
     return {
         'EU_STRATEGY': getattr(settings, 'EU_STRATEGY', False),
         'SITE_HEADER': getattr(settings, 'SITE_HEADER', 'NBSAP'),
+        'DEFAULT_LANGUAGE': settings.LANGUAGE_CODE,
     }
