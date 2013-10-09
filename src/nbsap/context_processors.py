8cf59b32017c5b247b328abea9d5259225cde6b0
Alex Morega <alex@grep.ro>
2013-10-09 12:50:02 +0300

    always redirect back to current page

--- src/nbsap/context_processors.py
+++ src/nbsap/context_processors.py
@@ -1,16 +1,8 @@
 from django.conf import settings
 import re
 
-pattern = re.compile(r"^/administration/")
-
 def nbsap_admin(request):
-    if re.search(pattern, request.path):
-        redirect_to = '/administration'
-    else:
-        redirect_to = '/'
-
     return {
         'EU_STRATEGY': getattr(settings, 'EU_STRATEGY', False),
         'SITE_HEADER': getattr(settings, 'SITE_HEADER', 'NBSAP'),
-        'redirect_to': redirect_to,
     }
