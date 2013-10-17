a2b000c41b85ea4aa03833bdba2f697b65f60887
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-10-17 17:31:33 +0300

    added webtest in settings

--- src/nbsap/settings.py
+++ src/nbsap/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django_assets',
+    'django_webtest',
     'transmeta',
     'django.contrib.markup',
     'tinymce',
