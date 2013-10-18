9c693b050599691d6efdb632d63880190338acc9
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-10-18 17:05:49 +0300

    populate_fields and normalize data

--- src/nbsap/tests/base.py
+++ src/nbsap/tests/base.py
@@ -2,4 +2,24 @@ from django_webtest import WebTest
 
 
 class BaseWebTest(WebTest):
-    pass
+
+    def populate_fields(self, form, data):
+        for field_name, field in form.field_order:
+            if field_name in data:
+                value = data[field_name]
+                if isinstance(value, Model):
+                    value = value.pk
+                if isinstance(field, MultipleSelect):
+                    if not isinstance(value, list):
+                        value = [value]
+                if isinstance(field, (Select, MultipleSelect)):
+                    field.force_value(value)
+                else:
+                    field.value = value
+        return form
+
+    def normalize_data(self, data):
+        for k, v in data.items():
+            if isinstance(v, Model):
+                data[k] = v.pk
+        return data
