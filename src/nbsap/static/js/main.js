f1d141a1dbcf07a1fa94d31edf0c58a96d1d3a75
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-12-02 18:18:05 +0200

    indicator modal

--- src/nbsap/static/js/main.js
+++ src/nbsap/static/js/main.js
@@ -2,4 +2,11 @@ $(function () {
    $('.change-lang').on('click', function () {
         $(this).parent().find('form').submit();
    });
+
+   var indicator_modal = $('#indicator_modal');
+   indicator_modal.on('hidden', function () {
+     indicator_modal.data('modal', false);
+     indicator_modal.find('.modal-body').html('');
+   });
+
 });
