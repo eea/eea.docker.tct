b2b973881d8f7592970c79f0da6fea3f5fc7a46b
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2013-12-18 16:15:18 +0200

    implement generic theme, including for IE8-IE10

--- src/nbsap/assets.py
+++ src/nbsap/assets.py
@@ -25,12 +25,25 @@ CSS_ASSETS = (
     'css/chosen.css',
 ) + settings.CSS_ASSETS
 
+IE8_CSS_ASSETS = (
+    'css/ie8.css',
+)
+
+IE9_CSS_ASSETS = (
+    'css/ie9.css',
+)
+
 
 js = Bundle(*JS_ASSETS, filters='jsmin', output='gen/packed.js')
 js_admin = Bundle(*JS_ADMIN_ASSETS, filters='jsmin', output='gen/admin_packed.js')
 
 css = Bundle(*CSS_ASSETS, filters='cssmin', output='gen/packed.css')
+ie8_css = Bundle(*IE8_CSS_ASSETS)
+ie9_css = Bundle(*IE9_CSS_ASSETS)
 
 register('js', js)
 register('js_admin', js_admin)
 register('css', css)
+register('ie8_css', ie8_css)
+register('ie9_css', ie9_css)
+
