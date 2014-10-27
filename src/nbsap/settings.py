2d47c582dd50d40af6995a08c39cffd30555119b
Dragos Catarahia <dragos@Dragoss-Air.edw.lan>
2014-10-27 15:09:30 +0200

    remove 404 errors for sentry

--- src/nbsap/settings.py
+++ src/nbsap/settings.py
@@ -231,7 +231,6 @@ except ImportError:
     pass
 
 if not DEBUG:
-    MIDDLEWARE_CLASSES += ('raven.contrib.django.middleware.Sentry404CatchMiddleware',)
     SENTRY_DSN = 'http://e58d6f13a36d419d81c1d1e44e5e2519:d67d512c5f4343c0aebcdca30bc64871@sentry.mojito.edw.ro/16'
 
 if 'test' in sys.argv:
