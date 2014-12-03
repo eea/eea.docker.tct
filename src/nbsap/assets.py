23ae09443a92ca7b1ce8027e2b2905fea4d826c8
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2014-12-03 18:44:24 +0200

    One big push

--- src/nbsap/assets.py
+++ src/nbsap/assets.py
@@ -2,11 +2,10 @@ from django_assets import Bundle, register
 from django.conf import settings
 
 JS_ASSETS = (
-    'js/jquery-1.7.1.min.js',
-    'js/jquery.uniform.js',
+    'js/jquery-1.11.1.min.js',
     'js/jquery.utils.js',
     'js/uri.js',
-    'bootstrap/js/bootstrap.js',
+    'bootstrap/js/bootstrap.min.js',
     'js/main.js',
 )
 
@@ -16,10 +15,7 @@ JS_ADMIN_ASSETS = (
 )
 
 CSS_ASSETS = (
-    'bootstrap/css/bootstrap.css',
-    'bootstrap/css/bootstrap-responsive.css',
-    'css/bootstrap-ui/jquery-ui-1.8.16.custom.css',
-    'css/bootstrap-ui/jquery.ui.1.8.16.ie.css',
+    'bootstrap/css/bootstrap.min.css',
     'css/style.css',
 ) + settings.CSS_ASSETS
 
--- src/nbsap/static/js/main.js
+++ src/nbsap/static/js/main.js
@@ -6,7 +6,73 @@ $(function () {
    var indicator_modal = $('#indicator_modal');
    indicator_modal.on('hidden', function () {
      indicator_modal.data('modal', false);
-     indicator_modal.find('.modal-body').html('');
+     indicator_modal.html('');
    });
 
 });
+
+function Modal(container) {
+    self = this;
+    self.container = container;
+    self.activeModal = null;
+
+    self.open = function(target) {
+        $(self).trigger({
+          'type': 'modalopen',
+          'target': target
+        })
+        self.activeModal = target;
+        $(target).addClass('active');
+        $('body').addClass('modal-open');
+    };
+
+    self.close = function() {
+        target = self.activeModal;
+        self.activeModal = null;
+        $(target).removeClass('active');
+        $('body').removeClass('modal-open');
+    };
+
+    // events - buttons
+    $('[data-modal]').on('click', function(e) {
+        e.preventDefault();
+        target = $(this).data('modal');
+        if (target == 'close') {
+            self.close();
+        } else {
+          href = $(this).attr('href');
+          if (href) {
+            $.ajax({
+              url: href,
+              context: document.body,
+              beforeSend: function() {
+                $(target).html('loading');
+              }
+            }).done(function(html) {
+              $(target).html(html);
+            });
+          }
+          self.open(target);
+        }
+    });
+    // events - close
+    $(self.activeModal).on('click', function(event) {
+        event.stopPropagation();
+    });
+    $(self.container).on('click', function() {
+        if (self.activeModal) {
+            self.close();
+        }
+    });
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27 && self.activeModal) {
+            self.close();
+        }
+    });
+}
+
+$(document).ready(function() {
+  modal = new Modal($('.modal-container')[0]);
+});
+
+
