8c673c1e341a5c735db7bba303e11769d90d712d
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2015-01-21 14:41:41 +0200

    [refactor] move some JS code and improve a jQuery selector

--- src/nbsap/static/js/object_delete.js
+++ src/nbsap/static/js/object_delete.js
@@ -1,9 +1,5 @@
-$(function () {
-  $('.messages').delay(1000).fadeOut();
-});
-
 $("form").on("submit", function (e) {
-  var message = $("button.btn-danger").data("message");
+  var message = $("form > button.btn-danger").data("message");
   e.preventDefault();
   if(confirm(message)){
     this.submit();
