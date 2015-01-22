ba67997f4b973df15119fbe7b4e4950ebc52d151
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2015-01-22 18:27:23 +0200

    [Eu Indicators] add the possibility to edit indicators

--- src/nbsap/urls.py
+++ src/nbsap/urls.py
@@ -141,6 +141,10 @@ urlpatterns = patterns('',
         'nbsap.views.edit_eu_indicator',
         name='edit_eu_indicator'),
 
+    url(r'^administration/indicators/(?P<pk>\d+)/edit/$',
+        'nbsap.views.edit_eu_indicator',
+        name='edit_eu_indicator'),
+
     url(r'^administration/objectives/$',
             'nbsap.views.list_national_objectives',
             name='list_national_objectives'),
