4fa24e0c5afa392d54059f32fb73fad39e22f64a
Alex Eftimie <alex@eftimie.ro>
2015-04-27 11:32:59 +0300

    [import/export] Fix fixtures between oldprod and master.

---
+++ src/nbsap/management/commands/fix_fixture.py
@@ -0,0 +1,41 @@
+import csv
+import json
+import cStringIO, codecs
+from bs4 import BeautifulSoup
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        filename = args[0]
+        objs = {}
+
+        with open(filename, 'r') as fin:
+            data = json.load(fin)
+
+            for obj in data:
+                if obj['model'] not in objs:
+                    objs[obj['model']] = {}
+                objs[obj['model']][obj['pk']] = obj
+
+            for _, obj in objs['nbsap.nationalstrategy'].items():
+                eu_actions = obj['fields'].pop('eu_actions')
+                for action in eu_actions:
+                    action_obj = objs['nbsap.euaction'][action]['fields']
+                    action_obj.setdefault('national_strategy', [])
+                    action_obj['national_strategy'].append(obj['pk'])
+                    
+                eu_targets = obj['fields'].pop('eu_targets')
+                for target in eu_targets:
+                    target_obj = objs['nbsap.eutarget'][target]['fields']
+                    target_obj.setdefault('national_strategy', [])
+                    target_obj['national_strategy'].append(obj['pk'])
+
+
+        data = []
+        for model, values in objs.items():
+            data.extend(values.values())
+
+        with open('fixed-' + filename, 'w') as fout:
+            json.dump(data, fout, indent=1)
+        self.stdout.write("Done")
