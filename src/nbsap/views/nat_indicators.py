f5aa18a469df79c95ffdd0d8c5039f023791fd3c
Ana Ion <ana.ion4@gmail.com>
2015-06-18 15:27:59 +0300

    [national_indicator_type] Remove the indicator_type field. Remove the code validator. Refs #17023

--- src/nbsap/views/nat_indicators.py
+++ src/nbsap/views/nat_indicators.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 
 from auth import auth_required
 from nbsap import models
