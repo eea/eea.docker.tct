3ea2f293dbc62a6db4b89cd2c4a1222814739670
Eduard Predescu <eduard.florin.predescu@gmail.com>
2016-08-23 17:46:56 +0300

    added styling for backdrop + modals

--- src/nbsap/static/js/main.js
+++ src/nbsap/static/js/main.js
@@ -1,85 +1,85 @@
 $(function () {
-  $('.change-lang').on('click', function () {
-    $(this).parent().find('form').submit();
-  });
+   $('.change-lang').on('click', function () {
+        $(this).parent().find('form').submit();
+   });
 
-  var indicator_modal = $('#indicator_modal');
-  indicator_modal.on('hidden', function () {
-    indicator_modal.data('modal', false);
-    indicator_modal.html('');
-  });
+   var indicator_modal = $('#indicator_modal');
+   indicator_modal.on('hidden', function () {
+     indicator_modal.data('modal', false);
+     indicator_modal.html('');
+   });
 
 });
 
 function Modal(container) {
-  self = this;
-  self.container = container;
-  self.activeModal = null;
-
-  self.open = function (target) {
-    $(self).trigger({
-      'type': 'modalopen',
-      'target': target
-    });
-    self.activeModal = target;
-    $(target).addClass('active');
-    $('body').addClass('modal-open');
-  };
-
-  self.close = function () {
-    target = self.activeModal;
+    self = this;
+    self.container = container;
     self.activeModal = null;
-    $(target).removeClass('active');
-    $('body').removeClass('modal-open');
-  };
 
-  // events - buttons
-  $('[data-modal]').on('click', function (e) {
-    e.preventDefault();
-    target = $(this).data('modal');
-    if (target == 'close') {
-      self.close();
-    } else {
-      href = $(this).attr('href');
-      if (href) {
-        $.ajax({
-          url: href,
-          context: document.body,
-          beforeSend: function () {
-            $(target).html('loading');
-          }
-        }).done(function (html) {
-          $(target).html(html);
+    self.open = function(target) {
+        $(self).trigger({
+          'type': 'modalopen',
+          'target': target
         });
-      }
-      self.open(target);
-    }
-  });
-  // events - close
-  $(self.activeModal).on('click', function (event) {
-    event.stopPropagation();
-  });
-  $(self.container).on('click', function () {
-    if (self.activeModal) {
-      self.close();
-    }
-  });
-  $(document).keyup(function (e) {
-    if (e.keyCode == 27 && self.activeModal) {
-      self.close();
-    }
-  });
+        self.activeModal = target;
+        $(target).addClass('active');
+        $('body').addClass('modal-open');
+    };
 
-  // TOC in goals was generating double ul - li markup
+    self.close = function() {
+        target = self.activeModal;
+        self.activeModal = null;
+        $(target).removeClass('active');
+        $('body').removeClass('modal-open');
+    };
 
-  // $('.toc ul').columnlist({
-  //     size : 4,
-  //     'class' : 'column-list',
-  //     incrementClass : 'column-list-'
-  // });
+    // events - buttons
+    $('[data-modal]').on('click', function(e) {
+        e.preventDefault();
+        target = $(this).data('modal');
+        if (target == 'close') {
+            self.close();
+        } else {
+          href = $(this).attr('href');
+          if (href) {
+            $.ajax({
+              url: href,
+              context: document.body,
+              beforeSend: function() {
+                $(target).html('loading');
+              }
+            }).done(function(html) {
+              $(target).html(html);
+            });
+          }
+          self.open(target);
+        }
+    });
+    // events - close
+    $(self.activeModal).on('click', function(event) {
+        event.stopPropagation();
+    });
+    $(self.container).on('click', function() {
+        if (self.activeModal) {
+            self.close();
+        }
+    });
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27 && self.activeModal) {
+            self.close();
+        }
+    });
+
+    // TOC in goals was generating double ul - li markup
+
+    // $('.toc ul').columnlist({
+    //     size : 4,
+    //     'class' : 'column-list',
+    //     incrementClass : 'column-list-'
+    // });
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   modal = new Modal($('.modal-container')[0]);
 });
 
@@ -92,8 +92,8 @@ function showDescription(name, textSelector, url, type, code, value) {
   $(selector).on('change', function () {
     var text = $(this).parents('.form-group').find(textSelector);
     text.html('');
-    $("option:selected", this).each(function () {
-      $.get(url.replace('1', this.value), function (data) {
+    $("option:selected", this).each(function() {
+      $.get(url.replace('1', this.value), function(data) {
         data = $.parseJSON(data)[0];
         if (code) {
           var title = '<h5>' + type + ' ' + data.code;
@@ -101,7 +101,7 @@ function showDescription(name, textSelector, url, type, code, value) {
             title += ': ' + data.title;
             title += '</h5>';
           }
-          if (data.title && !data.value) {
+          if (data.title && !data.value){
             title += '</h5>';
             title += '<p>' + data.title + '</p>';
           }
@@ -137,7 +137,7 @@ function showActionCodeValue(name, textSelector, url) {
 
 function forbidChoicesIntersection(selector1, selector2) {
   $(selector1).on('change', function () {
-    $("option", this).each(function () {
+    $("option", this).each(function() {
       var option2 = $(selector2).find('option[value=' + this.value + ']');
       if (this.selected) {
         option2.hide();
@@ -157,7 +157,3 @@ function forbidCrossChoicesIntersection(name, nameOther) {
   forbidChoicesIntersection(selectorTargets, selectorOtherTargets);
   forbidChoicesIntersection(selectorOtherTargets, selectorTargets);
 }
-
-
-
-
