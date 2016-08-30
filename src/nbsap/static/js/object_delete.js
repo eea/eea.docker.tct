6eb54b9cec246e637bdf8cfe2dcee835cc97ab87
Eduard Predescu <eduard.florin.predescu@gmail.com>
2016-08-30 12:33:15 +0300

    correct context prompt message

--- src/nbsap/static/js/object_delete.js
+++ src/nbsap/static/js/object_delete.js
@@ -1,5 +1,5 @@
 $(".delete").on("submit", function (e) {
-  var message = $(".edit").data("message");
+  var message = $(this).find(".edit").data("message");
   e.preventDefault();
   if(confirm(message)){
     this.submit();
