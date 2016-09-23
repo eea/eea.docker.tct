85d915f101f3624bce3a762ccf46c7cbb8b7f7b4
Eduard Predescu <eduard.florin.predescu@gmail.com>
2016-09-23 10:54:36 +0300

    added fancy loader for ajax

--- src/nbsap/static/js/jquery.ajaxGetForm.js
+++ src/nbsap/static/js/jquery.ajaxGetForm.js
@@ -1,23 +1,34 @@
 (function($) {
-        $.ajaxGet = function(options) {
-            var settings = $.extend({
-                form: ".form",
-                button: ".ajaxget"
-            }, options);
+  $.ajaxSetup({
+    beforeSend: function() {
+      $('.loader-container').show();
+    },
+    complete: function() {
+      $('.loader-container').hide();
+    },
+    success: function() {
+      $('.loader-container').hide();
+    }
+  });
+  $.ajaxGet = function(options) {
+    var settings = $.extend({
+      form: ".form",
+      button: ".ajaxget"
+    }, options);
 
-            $(settings.button).on('click', function() {
-                var url = $(this).data('url');
-                $(settings.form).empty();
-                $.get(url, function(data) {
-                    $(settings.form)
-                        .append(data)
-                        .find('form')
-                        .attr('action', url);
-                })
-                $('html, body').animate({
-                    scrollTop: $(settings.form).offset().top
-                }, 800);
-            });
-        }
+    $(settings.button).on('click', function() {
+      var url = $(this).data('url');
+      $(settings.form).empty();
+      $.get(url, function(data) {
+        $(settings.form)
+          .append(data)
+          .find('form')
+          .attr('action', url);
+      })
+      $('html, body').animate({
+        scrollTop: $(settings.form).offset().top
+      }, 800);
+    });
+  }
 
 })(jQuery);
