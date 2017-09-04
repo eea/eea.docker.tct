24cb591b0778cf1281d14c3e261ea0d272c884f2
Andreea Dima <andreeadima95@gmail.com>
2017-09-04 17:19:45 +0300

    [refs #87901] Fix tests

--- tct/test_settings.py
+++ tct/test_settings.py
@@ -1,3 +1,5 @@
+from tct.settings import *
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -6,6 +8,11 @@ DATABASES = {
 
 NAT_STRATEGY = True
 EU_STRATEGY = True
+SECRET_KEY = 'test-secret'
+
+MIGRATION_MODULES = {
+    'tct': None,
+}
 
 import logging
 logging.getLogger("factory").setLevel(logging.ERROR)
