d0cf98fda9a497a9018443ac00f9774609207758
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2017-09-15 15:07:35 +0300

    remove GA domain

--- tct/context_processors.py
+++ tct/context_processors.py
@@ -28,11 +28,9 @@ def tct_navbar_link(request):
 
 def google_analytics(request):
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
-    ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
     if not settings.DEBUG and ga_prop_id:
         return {
             'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id,
-            'GOOGLE_ANALYTICS_DOMAIN': ga_domain,
         }
     return {}
 
